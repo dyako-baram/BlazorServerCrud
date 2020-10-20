81e9fc46fb0f6db078126a581783e62c92d0a739
Dyako Baram <55919885+dyako-baram@users.noreply.github.com>
2020-10-20 16:28:16 +0300

    Add project files.

---
+++ BlazorServerUi/Data/PersonCrud.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerUi.Data
+{
+    public class PersonCrud
+    {
+        private readonly IConfiguration _configuration;
+
+        public PersonCrud(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+    }
+}
---
+++ BlazorServerUi/Models/PersonModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerUi.Models
+{
+    public class PersonModel
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Full Name is Required")]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+
+        [Required]
+        [Display(Name = "Date Of Birth")]
+        [DataType(DataType.DateTime)]
+        public DateTime DOB { get; set; }
+
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Required]
+        public string Gender { get; set; }
+
+    }
+}
