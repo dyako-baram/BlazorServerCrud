e83938ce7f79eb987b32fdd6fc72a550a2c77f86
Dyako Baram <55919885+dyako-baram@users.noreply.github.com>
2020-10-20 19:47:01 +0300

    Adding Api Placeholder

---
+++ BlazorServerUi/Models/ApiModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerUi.Models
+{
+    public class ApiModel
+    {
+        //https://jsonplaceholder.typicode.com/posts
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+    }
+}
